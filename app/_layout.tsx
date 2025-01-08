35e13c24033586d521c3166b330518a66a276d22
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-01-08 14:41:56 +0100

    fix: mise à jour des layouts et configurations

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,16 @@
-import '../global.css';
-
-import { Stack } from 'expo-router';
+// Layout.tsx
+import React from 'react';
+import { ScrollView, View, Text, StyleSheet } from 'react-native';
+import { Slot } from 'expo-router';
+import Navbar from '~/components/Navbar';
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <View style={{ flex: 1 }}>
+      <ScrollView contentContainerStyle={{ flex: 1 }}>
+        <Slot />
+      </ScrollView>
+      <Navbar />  
+    </View>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,51 @@
-import { Stack, Link } from 'expo-router';
-
-import { Button } from '~/components/Button';
-import { Container } from '~/components/Container';
-import { ScreenContent } from '~/components/ScreenContent';
+import React from 'react';
+import { View, Text, Image, Button, StyleSheet, ScrollView } from 'react-native';
+import { Link } from 'expo-router';
+const BodyTracker = require('../assets/vt7r6you.bmp');
 
 export default function Home() {
   return (
-    <>
-
-      <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home" />
-        {/* <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link> */}
-        <Link href={{ pathname: '/login'}} asChild>
-          <Button title="Login" />
-        </Link>
-      </Container>
-    </>
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image
+          source={BodyTracker} 
+          style={styles.image}
+        />
+      </View>
+      <Text style={styles.description}>
+        Welcome to our app! Join now and enjoy all the great features we have to offer.
+      </Text>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#f4f4f4',
+  },
+  imageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  image: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+  },
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginBottom: 30,
+    color: '#333',
+  },
+  buttonsContainer: {
+    width: '100%',
+    paddingHorizontal: 50,
+    gap: 10,
+  },
+});
